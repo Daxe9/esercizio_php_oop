5a972d5be38fe120bc04add8701ea0a7da464c67
lai2-24 <lai2-24@itismeucci.com>
2024-01-31 13:27:39 +0100

    init

---
+++ Alunno.php
@@ -0,0 +1,32 @@
+<?php
+class Alunno {
+    protected $name;
+    protected $surname;
+    protected $age;
+
+    public function __construct($name, $surname, $age) {
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->age = $age;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getSurname() {
+        return $this->surname;
+    }
+    public function setSurname($surname) {
+        $this->name = $surname;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+    public function setAge($age) {
+        $this->name = $age;
+    }
+}
